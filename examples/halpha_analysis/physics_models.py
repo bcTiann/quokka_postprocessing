cc081530054daee51762ccde48ca974b355ab6a5
bcTiann <baochen.tian.phys@gmail.com>
2025-11-19 17:34:47 +1100

    🚀 Quick Save: 2025-11-19 17:34:47

--- examples/halpha_analysis/physics_models.py
+++ examples/halpha_analysis/physics_models.py
@@ -6,6 +6,8 @@ from yt.units import K, mp, kb, mh, planck_constant, cm, m, s, g, erg
 import quokka2s as q2s
 from quokka2s.despotic_tables import compute_average
 import config as cfg
+from quokka2s.tables.lookup import TableLookup
+from examples.halpha_analysis.run_despotic import load_table
 
 
 # --- Fundamental Physical Constants ---
@@ -14,29 +16,29 @@ lambda_Halpha = 656.3e-7 * cm
 h = planck_constant
 speed_of_light_value_in_ms = 299792458 
 c = speed_of_light_value_in_ms * m / s
-TABLE_LOOKUP_CACHE: dict[str, np.ndarray] | None = None
+TABLE_LOOKUP_CACHE: TableLookup | None = None
 TABLE_LOOKUP_SPECIES: tuple[str, ...] = ()
 
+def ensure_table_lookup(path: str | None) -> TableLookup:
+    global TABLE_LOOKUP_CACHE
+    if TABLE_LOOKUP_CACHE is None:
+        table = load_table(path or cfg.DESPOTIC_TABLE_PATH)
+        TABLE_LOOKUP_CACHE = TableLookup(table)
+    return TABLE_LOOKUP_CACHE
 
 
-
-def number_density_H(field, data):
-    density_3d, extent = provider.get_slab_z(("gas", "density"))
-    dx_3d, _ = provider.get_slab_z(("boxlib", "dx"))
-    dy_3d, _ = provider.get_slab_z(("boxlib", "dy"))
-    dz_3d, _ = provider.get_slab_z(("boxlib", "dz"))
-
+def _number_density_H(field, data):
+    density_3d = data[('gas', 'density')].in_cgs()
     n_H_3d = (density_3d * cfg.X_H) / m_H
-
     return n_H_3d.to('cm**-3')
 
 
-def column_density_H(field, data):
-    density_3d, extent = provider.get_slab_z(("gas", "density"))
-    dx_3d, _ = provider.get_slab_z(("boxlib", "dx"))
-    dy_3d, _ = provider.get_slab_z(("boxlib", "dy"))
-    dz_3d, _ = provider.get_slab_z(("boxlib", "dz"))
-    
+def _column_density_H(field, data):
+    density_3d = data[('gas', 'density')].in_cgs()
+    dx_3d = data[("boxlib", "dx")].in_cgs()
+    dy_3d = data[("boxlib", "dy")].in_cgs()
+    dz_3d = data[("boxlib", "dz")].in_cgs()
+
     n_H_3d = (density_3d * cfg.X_H) / m_H
 
     Nx_p = q2s.along_sight_cumulation(n_H_3d * dx_3d, axis="x", sign="+")
@@ -55,37 +57,36 @@ def column_density_H(field, data):
 # --- YT Derived Fields ---
 
 
-
-def _temp_neutral(field, data):
-    """
-    (内部辅助字段)
-    计算假设 mu=1.0 时的“中性温度”(T_neutral 或 temp_init)
-    """
-    mu = 1.0
-    eint = data[('gas', 'internal_energy_density')].in_cgs()
-    rho = data[('gas', 'density')].in_cgs()
-    
-    # number density (假设 mu=1.0)
-    n = rho / (mu * mp)
-    
-    temp_init = (2.0 / 3.0) * eint / (n * kb)
-    return temp_init.to('K')
-
-
 def _temperature(field, data):
-    temp_init = data[('gas', 'temp_neutral')]
-    high_temp_mask = temp_init > (1.0e4 * K)
+    n_H = data[('gas', 'number_density_H')]
+    colDen_H = data[('gas', 'column_density_H')]
+    # for each point (nH, colDen) we get lookup our despotic table, get tempearture for that (nH, colDen)
+    lookup = ensure_table_lookup(cfg.DESPOTIC_TABLE_PATH)
 
-    corrected_temp = temp_init.copy()
-    corrected_temp[high_temp_mask] = temp_init[high_temp_mask] * 0.5
+    temps = lookup.temperature(nH_cgs=n_H.value, colDen_cgs=colDen_H.value)
 
-    return corrected_temp.to('K')
+    return temps * K
 
-def _ionized_mask(field, data):
-    temp_init = data[('gas', 'temp_neutral')]
-    mask = (temp_init > (1.0e4 * K)).astype('float64')
-    return mask
 
+# def _number_density_electron(field, data):
+#     n_H = data[('gas', 'number_density_H')]
+#     colDen_H = data[('gas', 'column_density_H')]
+#     lookup = ensure_table_lookup(cfg.DESPOTIC_TABLE_PATH)
+#     number_density_electron = lookup.number_densities('e-', nH_cgs=n_H.value, colDen_cgs=colDen_H.value)
+
+#     return number_density_electron * cm**-3
+
+def _make_number_density_field(species: str):
+    lookup = ensure_table_lookup(cfg.DESPOTIC_TABLE_PATH)
+    token = species
+    yt_safe_name = species.replace('+', '_plus').replace('-','_minus')
+    def _field(field, data):
+        n_H = data[('gas','number_density_H')].to('cm**-3').value
+        colDen = data[('gas','column_density_H')].to('cm**-2').value
+        densities = lookup.number_densities([token], n_H, colDen)
+        return densities[token] * cm**-3
+    _field.__name__ = f"_number_density_{yt_safe_name}"
+    return yt_safe_name, _field
 
 
 def _Halpha_luminosity(field, data):
@@ -96,18 +97,12 @@ def _Halpha_luminosity(field, data):
     Draine (2011) Eq. 14.6
     """
     E_Halpha = (h * c) / lambda_Halpha # Energy of a single H-alpha photon
-    rho = data[('gas', 'density')].in_cgs()
+    density_3d = data[('gas', 'density')].in_cgs()
     temp = data[('gas', 'temperature')].in_cgs()
-    # dx = data[('boxlib', 'dx')].in_cgs()
-    # dy = data[('boxlib', 'dy')].in_cgs()
-    # dz = data[('boxlib', 'dz')].in_cgs()
-    # V_cell = dx * dy * dz
-    ionized_mask = data[('gas', 'ionized_mask')]
-
-
-    n_H = rho / mh
-    n_e = n_H * ionized_mask
-    n_ion = n_e # hold only for fully ionized H gas
+   
+    n_e = data[('gas', 'e-')]
+    n_ion = data[('gas', 'H+')]
+    n_H = (density_3d * cfg.X_H) / m_H
 
     Z = 1.0
     T4 = temp / (1e4 * yt.units.K)
@@ -125,9 +120,21 @@ def _Halpha_luminosity(field, data):
 
 def add_all_fields(ds):
     """Adds all derived fields to the yt dataset."""
-    ds.add_field(name=('gas', 'temp_neutral'), function=_temp_neutral, sampling_type="cell", units="K", force_override=True)
+    ds.add_field(name=('gas', 'number_density_H'), function=_number_density_H, sampling_type="cell", units="cm**-3", force_override=True)
+    ds.add_field(name=('gas', 'column_density_H'), function=_column_density_H, sampling_type="cell", units="cm**-2", force_override=True)
     ds.add_field(name=('gas', 'temperature'), function=_temperature, sampling_type="cell", units="K", force_override=True)
-    ds.add_field(name=('gas', 'ionized_mask'), function=_ionized_mask, sampling_type="cell", units='', force_override=True)
+    
+    SPECIES = ['H+', 'H2', 'H3+', 'He+', 'OHx', 'CHx', 'CO', 'C',
+           'C+', 'HCO+', 'O', 'M+', 'H', 'He', 'M', 'e-']
+    for sp in SPECIES:
+        _, func = _make_number_density_field(species=sp)
+        ds.add_field(
+            name=('gas', f'{sp}'),
+            function=func,
+            sampling_type="cell", 
+            units="cm**-3", 
+            force_override=True
+        )
     ds.add_field(name=('gas', 'Halpha_luminosity'), function=_Halpha_luminosity, sampling_type="cell", units="erg/s/cm**3", force_override=True)
     print("Added derived fields: 'temp_neutral', 'temperature', 'ionized_mask', 'Halpha_luminosity'.")
 
